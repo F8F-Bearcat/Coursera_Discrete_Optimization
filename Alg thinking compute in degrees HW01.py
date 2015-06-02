43c1e87ca5478df3c2f9e2a1cdc0d95b64d2e237
F8F-Bearcat <debaets@hotmail.com>
2015-06-02 09:24:40 -0700

    add function to compute histogram dictionary

--- Alg thinking compute in degrees HW01.py
+++ Alg thinking compute in degrees HW01.py
@@ -28,7 +28,7 @@ def make_complete_graph(num_nodes):
 def compute_in_degrees(digraph):
     '''
     Input: a dictionary representing a directed graph. Nodes are keys, edges are values in a set
-    Output: a dictionary of nodes as keys with values equal to the sum of directed edges
+    Output: a dictionary of nodes as keys with values equal to a set including the directed edges
     pointing at that node.
     '''
     if len(digraph) == 0:
@@ -45,7 +45,20 @@ def compute_in_degrees(digraph):
                 pointed_to[item] = temp_set
     return pointed_to
 
+def in_degree_distribution(digraph):
+    '''
+    Input: a dictionary representing a directed graph. Nodes are keys, edges are values in a set
+    Output: a dictionary of nodes as keys with values equal to the integer sum of the edges pointing to the node
+    dictionary naming convention trailing _d denotes dictionary
+    '''
+    if len(digraph) == 0:
+        return {}
 
+    histogram_data_d = {}
+    for ele in digraph:
+        histogram_data_d[ele] = len(digraph[ele])
+
+    return histogram_data_d
 
 EX_GRAPH0 = {0:set([1, 2]), 1:set([]), 2:set([])}
 EX_GRAPH1 = {0:set([1, 4, 5]), 1:set([2, 6]), 2:set([3]), 3:set([0]), 4:set([1]), 5:set([2]), 6:set([])}
@@ -63,8 +76,10 @@ EX_GRAPH2[9] = set([0, 3, 4, 5, 6, 7])
 
 print EX_GRAPH2
 
-result_d = compute_in_degrees(EX_GRAPH0)
+result_d = compute_in_degrees(EX_GRAPH2)
 print result_d
+print in_degree_distribution(result_d)
+#
 print 'node', 'in_degree'
 for ele in result_d:
     print ele, '    ', len(result_d[ele])
