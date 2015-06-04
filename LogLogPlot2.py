0367e53c016392bcfd13c92956333f550b69e111
F8F-Bearcat <debaets@hotmail.com>
2015-06-04 15:28:47 -0700

    LogLogPlot2.py seems to be what I want

---
+++ LogLogPlot2.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#plt.subplots_adjust(hspace=.4)
+t = np.arange(0.01, 20.0, 0.01)
+
+# log x and y axis
+#plt.loglog(t, 20*np.exp(-t/10.0), basex=10)
+plt.loglog([1,2,3,10,12],[.5, 5, 50, 500, 520], 'r--', basex=10)  # 'ro' can be removed for default blue line
+plt.grid(True)
+plt.xlabel('In-Degree')
+plt.ylabel('Normalized')
+plt.title('loglog base 10 on X & Y axis')
+
+plt.show()
