7bc836efd8661111586b0d7a41e7534fe5164a48
F8F-Bearcat <debaets@hotmail.com>
2015-06-26 17:51:35 -0700

    Initial code for Algor Thinking  Hw #2
    candidate code for 'visited' function

---
+++ AlgThHW02.py
@@ -0,0 +1,37 @@
+'''
+Graph datastructure work.  Starting with the bfs (breadth first search) algorithm. And then
+cc (connected components) will be added
+'''
+from collections import deque
+
+def bfs_visited(ugraph, start_node):
+    '''
+    Input:  ugraph, an undirected graph.  start_node, to node to start the search for all
+            connected nodes. Assume ugraph has no duplicate edges w no start/end on same node
+
+    Output: a set of all the nodes visited by the algorithm
+    '''
+    if ugraph == set([]):
+        return set([])
+    queue = deque()
+    visited = set([start_node])
+    queue.append(start_node)            # enter the queue from the right
+
+    while len(queue) > 0:
+        jay = queue.popleft()           # remove elements from the left
+        neighbor_nodes = ugraph[jay]
+        for element in neighbor_nodes:
+            if element not in visited:
+                visited = visited | set([element])
+                queue.append(element)
+
+    return visited
+
+
+
+EX_GRAPH0 = {0:set([1, 2]), 1:set([]), 2:set([])}
+EX_GRAPH1 = {0:set([1, 4, 5]), 1:set([2, 6]), 2:set([3]), 3:set([0]), 4:set([1]), 5:set([2]), 6:set([])}
+'''
+EX_GRAPH2 = {0: set([1, 4, 5]), 1: set([2, 6]), 2: set([3, 7]), 3: set([7]), 4: set([1]), 5: set([2]), 6: set([]), 7: set([3]), 8: set([1, 2]), 9: set([0, 3, 4, 5, 6, 7])}
+'''
+print bfs_visited(EX_GRAPH0, 0)
