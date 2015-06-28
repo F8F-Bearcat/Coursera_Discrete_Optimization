d2165b69edd4264fad1be3232fca6fcb0132b3e8
andyd <debaets@hotmail.com>
2015-06-28 08:40:22 -0700

    Oops, too many changes at once for HW02 Applications

--- AlgHW01_helper.py
+++ AlgHW01_helper.py
@@ -8,31 +8,6 @@ of DPA algorithm
 # general imports
 import random
 
-def make_rand_digraph(num_nodes, prob):
-    '''
-    Make a random directed graph
-    Input: number of nodes, probability that an edge exists
-    Output: A dictionary with keys as node numbers and values as sets of nodes pointed
-    to by the key node
-    Edge (i,j) is an edge going from node i to node j, head of edge at node j
-    '''
-    rand_graph_d = {}
-    range_i = range(num_nodes)
-    range_j = range(num_nodes)
-    for ele in range_i:
-        rand_graph_d[ele] = set([])     #initialize graph with values as empty sets for each node
-
-    # generate the edges, iterate through i and j
-    for eye in range_i:
-        for jay in range_j:
-            if eye != jay:
-                if random.random() < prob:     # random.random() returns a float between 0 and 1
-                    temp_set = rand_graph_d[eye]
-                    temp_set = temp_set | set([jay])
-                    rand_graph_d[eye] = temp_set
-
-    return rand_graph_d
-
 class DPATrial:
     """
     Simple class to encapsulate optimized trials for DPA algorithm
@@ -82,9 +57,34 @@ class DPATrial:
         self._num_nodes += 1
         return new_node_neighbors
 
-    # Make a complete random directed graph with 13 nodes 
-    dpa_graph = make_rand_digraph(13, 1)
-    print dpa_graph
+def make_rand_digraph(num_nodes, prob):
+    '''
+    Make a random directed graph
+    Input: number of nodes, probability that an edge exists
+    Output: A dictionary with keys as node numbers and values as sets of nodes pointed
+    to by the key node
+    Edge (i,j) is an edge going from node i to node j, head of edge at node j
+    '''
+    rand_graph_d = {}
+    range_i = range(num_nodes)
+    range_j = range(num_nodes)
+    for ele in range_i:
+        rand_graph_d[ele] = set([])     #initialize graph with values as empty sets for each node
 
-    trial_obj = DPATrial(13)
-    print trial_obj
+    # generate the edges, iterate through i and j
+    for eye in range_i:
+        for jay in range_j:
+            if eye != jay:
+                if random.random() < prob:     # random.random() returns a float between 0 and 1
+                    temp_set = rand_graph_d[eye]
+                    temp_set = temp_set | set([jay])
+                    rand_graph_d[eye] = temp_set
+
+    return rand_graph_d
+
+# Make a complete random directed graph with 13 nodes 
+dpa_graph = make_rand_digraph(13, 1)
+print dpa_graph
+
+trial_obj = DPATrial(13)
+print trial_obj
