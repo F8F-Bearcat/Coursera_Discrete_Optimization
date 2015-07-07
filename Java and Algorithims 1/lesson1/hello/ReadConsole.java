3fc6b65f941be7d0956d603baf20efe1ec23bfd0
F8F-Bearcat <debaets@hotmail.com>
2015-07-07 16:35:25 -0700

    Percolate test files

--- Java and Algorithims 1/lesson1/hello/ReadConsole.java
+++ Java and Algorithims 1/lesson1/hello/ReadConsole.java
@@ -1,10 +1,14 @@
 import java.util.Scanner;
 
+
 public class ReadConsole
 {
+    private WeightedQuickUnionUF QF = new WeightedQuickUnionUF(10);
+    
     public static void main(String[] args)
     {
         System.out.println("Hello, World!");
+        System.out.println(QF.count());
         Scanner in = new Scanner(System.in);
         System.out.println("Scanner output here:");
         String text = in.nextLine();
