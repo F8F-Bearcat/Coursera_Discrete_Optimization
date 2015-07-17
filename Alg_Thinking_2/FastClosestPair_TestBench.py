2f7cdb21ca890153396ec87db2539130825b6697
F8F-Bearcat <debaets@hotmail.com>
2015-07-16 17:41:19 -0700

    Initial testbench construction

---
+++ Alg_Thinking_2/FastClosestPair_TestBench.py
@@ -0,0 +1,24 @@
+'''
+This is a test bench environment to verify FastClosestPair implementations.
+Class:  Algorithmic Thinking Part 2
+Coursera, Rice University
+Builds a source of random points, then applies to both the FastClosestPair and a brute force design
+The brute force design is viewed as the 'golden reference', easy to implement, but slower
+Run millions of verification cases to make sure FastClosestPair delivers the same results
+'''
+import random
+import cluster_class
+
+seed = 1        # set random seed so results can be reproducible
+size = 20       # number of points generated
+loops = 100     # number of  test cases generated and checked
+control_vector = [seed, size, loops]
+
+all_points = []
+for item in range(size):    # make random test points in area bounded by +/- 1 in x,y  0,0 center
+    point_x = 2*random.random() - 1
+    point_y = 2*random.random() - 1
+    point = (point_x, point_y)
+    all_points.append(point)
+
+print all_points
