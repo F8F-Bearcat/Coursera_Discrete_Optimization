b2b2da3b12f55d81ca8149a13a97ec4eb89f9ea3
F8F-Bearcat <debaets@hotmail.com>
2015-07-16 18:31:24 -0700

    Test bench loop set up working on plotting scatter graph now

--- Alg_Thinking_2/FastClosestPair_TestBench.py
+++ Alg_Thinking_2/FastClosestPair_TestBench.py
@@ -8,17 +8,30 @@ Run millions of verification cases to make sure FastClosestPair delivers the sam
 '''
 import random
 import cluster_class
+import SlowClosestPair
 
 seed = 1        # set random seed so results can be reproducible
 size = 20       # number of points generated
-loops = 100     # number of  test cases generated and checked
+loops = 1000     # number of  test cases generated and checked
 control_vector = [seed, size, loops]
 
-all_points = []
-for item in range(size):    # make random test points in area bounded by +/- 1 in x,y  0,0 center
-    point_x = 2*random.random() - 1
-    point_y = 2*random.random() - 1
-    point = (point_x, point_y)
-    all_points.append(point)
+for cycle in range(loops):
 
-print all_points
+    all_points = []
+    for item in range(size):    # make random test points in area bounded by +/- 1 in x,y  0,0 center
+        point_x = 2*random.random() - 1
+        point_y = 2*random.random() - 1
+        point = (point_x, point_y)
+        all_points.append(point)
+
+    #print all_points
+
+    cluster_list = []
+    for point in all_points:
+        first_cluster = cluster_class.Cluster(95014, point[0], point[1], 100, .5)
+        cluster_list.append(first_cluster)
+
+    slow_distance = SlowClosestPair.SlowClosestPair(cluster_list)
+
+    print slow_distance
+
--- Alg_Thinking_2/SlowClosestPair.py
+++ Alg_Thinking_2/SlowClosestPair.py
@@ -5,132 +5,7 @@ This script implements the SlowClosestPair function from Homework 3, Project 3
 import math
 import itertools
 import random
-
-class Cluster:
-    """
-    Class for creating and merging clusters of counties
-    """
-    
-    def __init__(self, fips_codes, horiz_pos, vert_pos, population, risk):
-        """
-        Create a cluster based the models a set of counties' data
-        """
-        self._fips_codes = fips_codes
-        self._horiz_center = horiz_pos
-        self._vert_center = vert_pos
-        self._total_population = population
-        self._averaged_risk = risk
-        
-        
-    def __repr__(self):
-        """
-        String representation assuming the module is "alg_cluster".
-        """
-        rep = "alg_cluster.Cluster("
-        rep += str(self._fips_codes) + ", "
-        rep += str(self._horiz_center) + ", "
-        rep += str(self._vert_center) + ", "
-        rep += str(self._total_population) + ", "
-        rep += str(self._averaged_risk) + ")"
-        return rep
-
-
-    def fips_codes(self):
-        """
-        Get the cluster's set of FIPS codes
-        """
-        return self._fips_codes
-    
-    def horiz_center(self):
-        """
-        Get the averged horizontal center of cluster
-        """
-        return self._horiz_center
-    
-    def vert_center(self):
-        """
-        Get the averaged vertical center of the cluster
-        """
-        return self._vert_center
-    
-    def total_population(self):
-        """
-        Get the total population for the cluster
-        """
-        return self._total_population
-    
-    def averaged_risk(self):
-        """
-        Get the averaged risk for the cluster
-        """
-        return self._averaged_risk
-   
-        
-    def copy(self):
-        """
-        Return a copy of a cluster
-        """
-        copy_cluster = Cluster(set(self._fips_codes), self._horiz_center, self._vert_center,
-                               self._total_population, self._averaged_risk)
-        return copy_cluster
-
-
-    def distance(self, other_cluster):
-        """
-        Compute the Euclidean distance between two clusters
-        """
-        vert_dist = self._vert_center - other_cluster.vert_center()
-        horiz_dist = self._horiz_center - other_cluster.horiz_center()
-        return math.sqrt(vert_dist ** 2 + horiz_dist ** 2)
-        
-    def merge_clusters(self, other_cluster):
-        """
-        Merge one cluster into another
-        The merge uses the relatively populations of each
-        cluster in computing a new center and risk
-        
-        Note that this method mutates self
-        """
-        if len(other_cluster.fips_codes()) == 0:
-            return self
-        else:
-            self._fips_codes.update(set(other_cluster.fips_codes()))
- 
-            # compute weights for averaging
-            self_weight = float(self._total_population)                        
-            other_weight = float(other_cluster.total_population())
-            self._total_population = self._total_population + other_cluster.total_population()
-            self_weight /= self._total_population
-            other_weight /= self._total_population
-                    
-            # update center and risk using weights
-            self._vert_center = self_weight * self._vert_center + other_weight * other_cluster.vert_center()
-            self._horiz_center = self_weight * self._horiz_center + other_weight * other_cluster.horiz_center()
-            self._averaged_risk = self_weight * self._averaged_risk + other_weight * other_cluster.averaged_risk()
-            return self
-
-    def cluster_error(self, data_table):
-        """
-        Input: data_table is the original table of cancer data used in creating the cluster.
-        
-        Output: The error as the sum of the square of the distance from each county
-        in the cluster to the cluster center (weighted by its population)
-        """
-        # Build hash table to accelerate error computation
-        fips_to_line = {}
-        for line_idx in range(len(data_table)):
-            line = data_table[line_idx]
-            fips_to_line[line[0]] = line_idx
-        
-        # compute error as weighted squared distance from counties to cluster center
-        total_error = 0
-        counties = self.fips_codes()
-        for county in counties:
-            line = data_table[fips_to_line[county]]
-            singleton_cluster = Cluster(set([line[0]]), line[1], line[2], line[3], line[4])
-            singleton_distance = self.distance(singleton_cluster)
-            total_error += (singleton_distance ** 2) * singleton_cluster.total_population()
-        return total_error
+import cluster_class
 
 def euclid_dist(pOne, pTwo):
     '''
@@ -162,17 +37,17 @@ def SlowClosestPair(list_of_clusters):
             ret_list[2] = list_of_clusters.index(item[1])
             assert list_of_clusters.index(item[0]) < list_of_clusters.index(item[1]), 'index order small to large'
     return tuple(ret_list)
-
-p01 = Cluster(95014, 0, 0, 100, .5)
-p02 = Cluster(95014, 0, 1, 100, .5)
-p03 = Cluster(95014, 1, 1, 100, .5)
-p04 = Cluster(95014, 1, 0, 100, .5)
-p05 = Cluster(95014, -1, 1, 100, .5)
-p06 = Cluster(95014, -1, 0, 100, .5)
-p07 = Cluster(95014, -1, -1, 100, .5)
-p08 = Cluster(95014, 0, -1, 100, .5)
-p09 = Cluster(95014, 1, -1, 100, .5)
-p10 = Cluster(95014, 1, 1.1, 100, .5)
+'''
+p01 = cluster_class.Cluster(95014, 0, 0, 100, .5)
+p02 = cluster_class.Cluster(95014, 0, 1, 100, .5)
+p03 = cluster_class.Cluster(95014, 1, 1, 100, .5)
+p04 = cluster_class.Cluster(95014, 1, 0, 100, .5)
+p05 = cluster_class.Cluster(95014, -1, 1, 100, .5)
+p06 = cluster_class.Cluster(95014, -1, 0, 100, .5)
+p07 = cluster_class.Cluster(95014, -1, -1, 100, .5)
+p08 = cluster_class.Cluster(95014, 0, -1, 100, .5)
+p09 = cluster_class.Cluster(95014, 1, -1, 100, .5)
+p10 = cluster_class.Cluster(95014, 1, 1.1, 100, .5)
 
 test_list = [p01, p02, p03, p04, p05, p06, p07, p08, p09, p10]
 
@@ -181,3 +56,4 @@ for element in range(100000):
     print SlowClosestPair(test_list)
 
 #print SlowClosestPair(test_list)
+'''
