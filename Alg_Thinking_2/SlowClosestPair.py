41f0c51886eab59eb5c98eea9411eb391fe449a4
F8F-Bearcat <debaets@hotmail.com>
2015-07-20 12:10:54 -0700

    working with cluster inputs

--- Alg_Thinking_2/SlowClosestPair.py
+++ Alg_Thinking_2/SlowClosestPair.py
@@ -5,7 +5,7 @@ and kmeans_clustering
 and meet pylint coding guidelines
 '''
 import math
-#import itertools
+import cluster_class
 
 def euclid_dist(p_one, p_two):
     '''
@@ -17,35 +17,38 @@ def euclid_dist(p_one, p_two):
     assert len(p_one) == 2, 'both inputs need two coordinates'
     return math.sqrt((p_one[0]-p_two[0])**2 + (p_one[1]-p_two[1])**2)
 
-def slow_closest_pair(list_of_points):
+def slow_closest_pair(list_of_clusters):
     '''
-    Input: a list of points
-    Output: a tuple (d, i, j) where d is the smallest between any of the points in list_of_points
+    Input: a list of clusters
+    Output: a tuple (d, i, j) where d is the smallest between any of the points in list_of_clusters
             and i and j are the indexes of the two points that are the closest together
     '''
     ret_list = [float('inf'), -1, -1]
-    #all_combinations_two_points = [list(x) for x in itertools.combinations(list_of_points, 2)]
-    all_combinations_two_points = []
-    for point in list_of_points:
-        if list_of_points.index(point) == len(list_of_points)-1:
+    #all_combinations_two_points = [list(x) for x in itertools.combinations(list_of_clusters, 2)]
+    all_combinations_two_clusters = []
+    for cluster in list_of_clusters:
+        if list_of_clusters.index(cluster) == len(list_of_clusters)-1:
             break
-        init_item_y = list_of_points.index(point)+1
-        for item_y in range(init_item_y, len(list_of_points)):
-            all_combinations_two_points.append((point, list_of_points[item_y]))
-    #print all_combinations_two_points
-    for item in all_combinations_two_points:
-        distance = euclid_dist(item[0], item[1])
+        next_cl_index = list_of_clusters.index(cluster)+1
+        for item_y in range(next_cl_index, len(list_of_clusters)):
+            all_combinations_two_clusters.append((cluster, list_of_clusters[item_y]))
+    #print all_combinations_two_clusters
+    for cluster_tuple in all_combinations_two_clusters:
+        points = []
+        for cluster in cluster_tuple:
+            points.append([cluster.horiz_center(), cluster.vert_center()])
+        distance = euclid_dist(points[0], points[1])
         if distance < ret_list[0]:   # if true, a new minimum distance has been found
             ret_list[0] = distance
-            ret_list[1] = list_of_points.index(item[0])
-            ret_list[2] = list_of_points.index(item[1])
-            low_index = list_of_points.index(item[0])
-            hi_index = list_of_points.index(item[1])
+            ret_list[1] = list_of_clusters.index(cluster_tuple[0])
+            ret_list[2] = list_of_clusters.index(cluster_tuple[1])
+            low_index = list_of_clusters.index(cluster_tuple[0])
+            hi_index = list_of_clusters.index(cluster_tuple[1])
             assert low_index < hi_index, 'index order small to large'
     return tuple(ret_list)
 '''
-p01 = cluster_class.Cluster(95014, 0, 0, 100, .5)
-p02 = cluster_class.Cluster(95014, 0, 1, 100, .5)
+p01 = cluster_class.Cluster(set([]), 0, 0, 100, .5)
+p02 = cluster_class.Cluster(set([]), 0, 1, 100, .5)
 p03 = cluster_class.Cluster(95014, 1, 1, 100, .5)
 p04 = cluster_class.Cluster(95014, 1, 0, 100, .5)
 p05 = cluster_class.Cluster(95014, -1, 1, 100, .5)
@@ -60,8 +63,11 @@ test_list = [p01, p02, p03, p04, p05, p06, p07, p08, p09, p10]
 for element in range(100000):
     random.shuffle(test_list)
     print SlowClosestPair(test_list)
-'''
+
 p01 = (0,0)
 p02 = (1,0)
+
+print p01.horiz_center()
 test_list = [p01, p02]
 print slow_closest_pair(test_list)
+'''
